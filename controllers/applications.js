1c889741011f580c560ebeb07661d2736e456d14
dbaping <p.cruz8@yahoo.com>
2026-01-08 21:14:21 -0400

    last commit

--- controllers/applications.js
+++ controllers/applications.js
@@ -46,6 +46,48 @@ router.get('/:applicationId', async (req, res) => {
     }
 })
 
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.applications.id(req.params.applicationId).deleteOne()
+        await currentUser.save() 
+        res.redirect('/users/${currentUser._id}/applications')
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.get('/:applicationId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/edit.ejs', {
+            application: application,
+        })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.put('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)  
+        const application = currentUser.applications.id(req.params.applicationId)
+        application.set(req.body)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`)
+
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+
+
+
 module.exports = router;
 
 
